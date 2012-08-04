3ab074fdb1ee345ed56f7f18677a7d9559cc757c
John Brogan <jbrogan58@gmail.com>
2012-08-04 08:16:36 -0400

    Update admin for sessions

--- permitTracker/admin.py
+++ permitTracker/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from models import Trainer, StateRequirement, Student, Session, State
 
+class StateRequirementAdmin(admin.ModelAdmin):
+    fieldsets = [
+	(None,		{'fields': ['state']}),
+	('State Requirements', {'fields': ['totalTime', 'totalNight', 'totalInclement']}),
+    ]
+
+
+class SessionAdmin(admin.ModelAdmin):
+    fieldsets = [
+	('Driver',	{'fields': ['studentName']}),
+	('Trainer',	{'fields': ['trainerName']}),
+	('Date',        {'fields': ['date']}),
+	('Driving',     {'fields': ['driveTime', 'distance', 'conditions']}),
+    ]
+    list_display = ('date', 'studentName', 'trainerName', 'driveTime', 'conditions')
+    list_filter = ['date', 'studentName', 'trainerName', 'conditions']
+    search_fields = ['studentName', 'trainerName']
+
 admin.site.register(Trainer)
-admin.site.register(StateRequirement)
+admin.site.register(StateRequirement, StateRequirementAdmin)
 admin.site.register(Student)
-admin.site.register(Session)
+admin.site.register(Session, SessionAdmin)
 admin.site.register(State)
